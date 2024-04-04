ea0f61d783b0945a32666e40a21c8f3fb143063a
ECarry <lianshiliang93@gmail.com>
2024-04-04 22:10:56 +0800

    init prisma

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
 
-// export default auth((req) => {
-//   // req.auth
-// });
+export default auth((req) => {
+  console.log("ROUTE:" + req.nextUrl);
+});
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
